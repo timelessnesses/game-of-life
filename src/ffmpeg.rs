4491e76bb06a64f4131d81f4095146b5976f7f0a
timelesnesses <timelessnesses@timelessnesses.me>
2024-03-24 14:47:59 +0700

    reducing ffmpeg code"

--- src/ffmpeg.rs
+++ src/ffmpeg.rs
@@ -1,47 +1,4 @@
-use std::{io::Write, path::PathBuf};
-
-struct IteratorThroughDiskReadThing {
-    current_index: usize,
-    files: Vec<PathBuf>,
-}
-impl Iterator for IteratorThroughDiskReadThing {
-    type Item = Vec<u8>;
-    fn next(&mut self) -> Option<Self::Item> {
-        self.current_index += 1;
-        if self.current_index - 1 >= self.files.len() {
-            return None;
-        }
-        let x = std::fs::read(self.files[self.current_index - 1].to_owned()).unwrap();
-        std::fs::remove_file(self.files[self.current_index - 1].to_owned()).unwrap();
-        return Some(x);
-    }
-}
-
-impl IteratorThroughDiskReadThing {
-    fn new(folder_name: String) -> Self {
-        let mut things = Vec::new();
-        let files = std::fs::read_dir(folder_name).unwrap();
-        let mut ordering = Vec::new();
-        for file in files {
-            ordering.push(file.unwrap().path());
-        }
-        ordering.sort_by_key(|x| {
-            x.to_str()
-                .unwrap()
-                .split('\\')
-                .last()
-                .and_then(|s| s.split(".").next())
-                .and_then(|n| n.parse::<i32>().ok())
-                .unwrap_or(0)
-        });
-        things.append(&mut ordering);
-        println!("{:#?}", things);
-        Self {
-            files: things,
-            current_index: 0,
-        }
-    }
-}
+use std::{self, io::Write};
 
 pub struct VideoRecorder {
     ffmpeg: std::process::Child,
@@ -49,7 +6,7 @@ pub struct VideoRecorder {
 
 impl VideoRecorder {
     pub fn new(out: &str, width: u32, height: u32, fps: u32) -> Self {
-        let mut ffmpeg_cmd = std::process::Command::new("ffmpeg")
+        let ffmpeg_cmd = std::process::Command::new("ffmpeg")
             .args([
                 "-f",
                 "rawvideo",
