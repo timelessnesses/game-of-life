5ba97f9f7041620802f0355241296b23278f36c5
timelesnesses <timelessnesses@timelessnesses.me>
2024-04-29 16:09:54 +0700

    aaaaaaa

--- android-project/app/jni/Android.mk
+++ android-project/app/jni/Android.mk
@@ -1,1 +1,34 @@
-include $(call all-subdir-makefiles)
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := main
+
+RELATIVE_LIBS_DIR := libs/$(TARGET_ARCH_ABI)
+LIB_NAME := libmain.so
+GLOBAL_LIBS_DIR := $(LOCAL_PATH)/$(RELATIVE_LIBS_DIR)
+LOCAL_SRC_FILES := $(RELATIVE_LIBS_DIR)/$(LIB_NAME)
+GLOBAL_LIB_FILE := $(GLOBAL_LIBS_DIR)/$(LIB_NAME)
+GLOBAL_VARS_FILE := $(GLOBAL_LIBS_DIR)/vars.sh
+CARGO_GLOBAL_PATH := $(LOCAL_PATH)
+
+LOCAL_SHARED_LIBRARIES := SDL2 SDL2_ttf
+
+$(shell mkdir -p $(GLOBAL_LIBS_DIR))
+$(shell touch $(GLOBAL_LIB_FILE))
+
+$(shell echo "TARGET_ARCH_ABI=\"$(TARGET_ARCH_ABI)\"" > $(GLOBAL_VARS_FILE))
+$(shell echo "TARGET_LIB=\"$(GLOBAL_LIB_FILE)\"" >> $(GLOBAL_VARS_FILE))
+$(shell echo "TARGET_ARCH=\"$(TARGET_ARCH)\"" >> $(GLOBAL_VARS_FILE))
+
+include $(PREBUILT_SHARED_LIBRARY)
+
+$(shell echo "TRIPLE=\"$(header_triple_$(TARGET_ARCH))\"" >> $(GLOBAL_VARS_FILE))
+$(shell echo "ANDROID_LDFLAGS=\"$(TARGET_LDFLAGS)\"" >> $(GLOBAL_VARS_FILE))
+$(shell echo "ANDROID_CC=\"$(TOOLCHAIN_PREFIX)gcc\"" >> $(GLOBAL_VARS_FILE))
+$(shell echo "ANDROID_LD_SYSROOT=\"$(SYSROOT_LINK)\"" >> $(GLOBAL_VARS_FILE))
+
+$(CARGO_GLOBAL_PATH)/libs/*/libmain.so : libs/*/libSDL2.so libs/*/libSDL2_image.so
+    $(CARGO_GLOBAL_PATH)/build-lib.sh "$@"
+
+$(shell rm $(GLOBAL_LIB_FILE))
--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #NOTE: Dont't forget to modify these vars to your setup
 JNI_LIBS=./android-project/app/src/main/jniLibs
 LIB_NAME=rust_game_of_life.so
-SDL2_LIBS=D:/mooping/SDL-release-2.26.x/libs
-BUILD_MODE=release
+SDL2_LIBS=D:/SDL-release-2.30.2/libs
+BUILD_MODE=
+ANDROID_NDK_HOME=D:/android-ndk-r26d
 
 #copy sdl2 libs into rusts build dir
 mkdir -p ./target/aarch64-linux-android/$BUILD_MODE/deps/
@@ -13,9 +14,8 @@ cp -a $SDL2_LIBS/armeabi-v7a/. target/armv7-linux-androideabi/$BUILD_MODE/deps/
 cp -a $SDL2_LIBS/x86/. ./target/i686-linux-android/$BUILD_MODE/deps/
 
 #build the libraries
-cargo build --target aarch64-linux-android --$BUILD_MODE
-cargo build --target armv7-linux-androideabi --$BUILD_MODE
-# cargo build --target i686-linux-android --$BUILD_MODE
+ANDROID_NDK_HOME=$ANDROID_NDK_HOME ANDROID_NDK=$ANDROID_NDK_HOME cargo build --target aarch64-linux-android --$BUILD_MODE
+ANDROID_NDK_HOME=$ANDROID_NDK_HOME ANDROID_NDK=$ANDROID_NDK_HOME cargo build --target armv7-linux-androideabi --$BUILD_MODE
 
 #prepare folders...
 rm -rf $JNI_LIBS
